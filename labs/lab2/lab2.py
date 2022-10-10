14d2456d437cae0614a1308aa624d877fcf0fe72
Duc Vo <dvo4@seattleu.edu>
2022-10-10 12:13:08 -0700

    lab2 - probing leader working

--- labs/lab2/lab2.py
+++ labs/lab2/lab2.py
@@ -4,11 +4,23 @@ import socket
 import pickle
 from datetime import datetime
 from enum import Enum
+import random
 
 BUF_SZ = 1024
 PEER_DIGITS = 100
-CHECK_INTERVAL = 1
+CHECK_INTERVAL = 0.001
 ASSUME_FAILURE_TIMEOUT = 2
+PROBING_DURATION = (500, 3000)
+ENABLE_PROBING = True
+
+
+class Color(Enum):
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    END = '\033[0m'
 
 
 class State(Enum):
@@ -21,6 +33,7 @@ class State(Enum):
     SEND_ELECTION = 'ELECTION'  # Keep connection to wait for response
     SEND_VICTORY = 'COORDINATOR'  # One way
     SEND_OK = 'OK'  # One way
+    SEND_PROBE = 'PROBE'
 
     # Incoming message is pending
     WAITING_FOR_OK = 'WAIT_OK'  # When I've sent them an ELECTION message
@@ -42,6 +55,7 @@ class Peer:
         self.bully = None
         self.selector = selectors.DefaultSelector()
         self.listener, self.listener_address = self.start_a_server()
+        self.leader_duration = 0.0
 
     def run(self):
         self.join_group()
@@ -62,16 +76,36 @@ class Peer:
 
             # if run out of time and NOT receive any OK
             self.check_timeouts()
+            ENABLE_PROBING and self.start_probing()
+
+    def start_probing(self):
+        if self.bully and self.bully != self.pid and self.is_leader_expired(self.leader_duration):
+            self.set_state(State.WAITING_FOR_OK, self.bully)
+            sock = self.get_connection(self.bully)
+            if sock:
+                self.set_state(State.SEND_PROBE, sock)
+                self.selector.register(sock, selectors.EVENT_WRITE)
+            else:
+                self.start_election('NO CONNECTION TO LEADER')
+
+    def is_leader_expired(self, threshold):
+        expired = False
+        state, timestamp = self.get_state(self.bully, True)
+        if state == State.SEND_PROBE:
+            duration = (datetime.now() - timestamp).total_seconds()
+            expired = duration > threshold
+        if expired:
+            print('> PROBING TIMEOUT [{}s]'.format(duration))
+        return expired
 
     def accept_peer(self):
-        """Callback for new connections"""
+        """Generate a connection for incoming request"""
         peer, peer_address = self.listener.accept()
-        print('ACCEPT PEER ({})'.format(peer_address))
         peer.setblocking(False)
         self.selector.register(peer, selectors.EVENT_READ)
 
     def join_group(self):
-        print('=== JOIN GROUP ===')
+        print('> JOIN GROUP')
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect(self.gcd_address)
             self.send(self, sock, 'JOIN', (self.pid, self.listener_address))
@@ -79,9 +113,10 @@ class Peer:
             self.update_members(data)
 
     def send_message(self, peer):  # selectors.EVENT_WRITE
-        print("=== SENDING MESSAGE ===")
+
         state = self.get_state(peer)
-        print('{}: OUTGOING {} [{}]'.format(self.pr_sock(self, peer), state.value, self.pr_now()))
+        print('{}: SEND → {}"{}"{} [{}]'
+              .format(self.pr_sock(self, peer), Color.WARNING.value, state.value, Color.END.value, self.pr_now()))
         try:
             self.send(self, peer, state.value, self.members)
         except Exception as e:
@@ -91,29 +126,33 @@ class Peer:
                 self.set_state(State.WAITING_FOR_OK, peer, True)
 
             if state == State.SEND_OK:
-                if not self.is_election_in_progress():
-                    self.start_election('NOT HAVE AN ELECTION IN PROGRESS')
                 self.set_quiescent(peer)
 
             if state == State.SEND_VICTORY:
                 self.set_state(State.WAITING_FOR_ANY_MESSAGE)
-                self.set_leader(self.pid)
                 self.set_quiescent(peer)
 
+            if state == State.SEND_PROBE:
+                self.set_state(State.WAITING_FOR_OK, peer, True)
+
     def receive_message(self, peer):  # selectors.EVENT_READ
-        print("=== RECEIVE MESSAGE ===")
-        state = self.get_state(peer)
-        print('{}: INCOMING {} [{}]'.format(self.pr_sock(self, peer), state.value, self.pr_now()))
         try:
             data = self.receive(peer)
         except Exception as e:
             print(e)
         else:
             message, members = data
-            print('"{}" RECEIVED'.format(message))
+            print('{}: RECV ← {}"{}"{} [{}]'
+                  .format(self.pr_sock(self, peer), Color.GREEN.value, message, Color.END.value, self.pr_now()))
+
             if message == State.SEND_OK.value:  # received OK
                 self.set_quiescent(peer)
-                self.set_state(State.WAITING_FOR_VICTOR)
+                if self.bully and self.pid != self.bully:
+                    self.leader_duration = self._pick_duration(PROBING_DURATION)
+                    self.set_state(State.SEND_PROBE, self.bully)
+
+                if not self.bully:
+                    self.set_state(State.WAITING_FOR_VICTOR)
 
             if message == State.SEND_ELECTION.value:  # received ELECTION
                 """
@@ -124,6 +163,9 @@ class Peer:
                 4. If you aren't in an election, then proceed as though you are initiating a new election.
                 """
                 self.update_members(members)
+                if not self.is_election_in_progress():
+                    self.bully = None
+                    self.start_election('NOT HAVE AN ELECTION IN PROGRESS')
                 self.set_state(State.SEND_OK, peer, True)  # switch to write
 
             if message == State.SEND_VICTORY.value:  # received COORDINATOR
@@ -131,42 +173,51 @@ class Peer:
                 for pid in members:
                     if pid[0] > bully[0] or pid[0] == bully[0] and pid[1] > bully[1]:
                         bully = pid
+                self.update_members(members)
                 self.set_leader(bully)
                 self.set_quiescent(peer)
                 self.set_state(State.WAITING_FOR_ANY_MESSAGE)
+
+            if message == State.SEND_PROBE.value:
+                self.set_state(State.SEND_OK, peer, True)
 
     def check_timeouts(self):
         if self.is_expired():
             self.declare_victory('TIMEOUT WAITING FOR OK')
 
-    @staticmethod
-    def get_connection(member):
+    def get_connection(self, member):
+        if member == self.pid:
+            return None
+
         try:
+            address = self.members[member]
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect(member)
+            sock.connect(address)
             sock.setblocking(False)
             return sock
         except Exception as e:
-            print(member, 'INACTIVE')
+            print(member, f'{Color.FAIL.value}INACTIVE{Color.END.value}')
             return None
 
     def is_election_in_progress(self):
         return self.get_state() in (State.WAITING_FOR_VICTOR, State.WAITING_FOR_OK)
 
     def is_expired(self, peer=None, threshold=ASSUME_FAILURE_TIMEOUT):
+        expired = False
         if peer is None:
             peer = self
         state, timestamp = self.get_state(peer, True)
         if state == State.WAITING_FOR_OK:
             duration = (datetime.now() - timestamp).total_seconds()
-            print(int(duration))
-            return duration > threshold
-        return False
+            expired = duration > threshold
+        if expired:
+            print('> OK TIMEOUT [{}s]'.format(duration))
+        return expired
 
     def set_leader(self, new_leader):
-        print("== NEW LEADER == ")
-        print("SELF {}".format(self.pid) if new_leader is self.pid else "OTHER {}".format(new_leader))
+        print("> LEADER IS", "SELF {}".format(self.pid) if new_leader is self.pid else "OTHER {}".format(new_leader))
         self.bully = new_leader
+        self.set_state(State.SEND_PROBE, new_leader)
 
     def get_state(self, peer=None, detail=False):
         """
@@ -189,10 +240,7 @@ class Peer:
         self.states[peer] = (state, datetime.now())
         if switch_mode:
             event = self.selector.get_key(peer).events
-            if event == selectors.EVENT_WRITE:
-                self.selector.modify(peer, selectors.EVENT_READ)
-            else:
-                self.selector.modify(peer, selectors.EVENT_WRITE)
+            self.selector.modify(peer, (1 << 0) if event == (1 << 1) else (1 << 1))
 
     def set_quiescent(self, peer=None):
         if peer is None:
@@ -203,12 +251,10 @@ class Peer:
         peer.close()
 
     def start_election(self, reason):
-        print('=== START ELECTION ===')
-        print('REASON: ', reason)
-
+        print('> START ELECTION ({})'.format(reason))
         for pid in self.members:
             if self.pid[0] < pid[0] or self.pid[0] == pid[0] and self.pid[1] < pid[1]:
-                sock = self.get_connection(self.members[pid])
+                sock = self.get_connection(pid)
                 if sock is None:
                     continue
                 self.set_state(State.SEND_ELECTION, sock)
@@ -217,8 +263,7 @@ class Peer:
         self.set_state(State.WAITING_FOR_OK)  # Use timeouts for bully as well
 
     def declare_victory(self, reason):
-        print('=== DECLARE VICTORY ===')
-        print('REASON: ', reason)
+        print('> DECLARE VICTORY ({})'.format(reason))
         members = {}
 
         for pid in self.members:
@@ -229,9 +274,7 @@ class Peer:
         self.set_state(State.WAITING_FOR_ANY_MESSAGE)
 
         for pid in self.members:
-            if pid == self.pid:
-                continue
-            sock = self.get_connection(members[pid])
+            sock = self.get_connection(pid)
             if sock is None:
                 continue
             self.set_state(State.SEND_VICTORY, sock)
@@ -240,17 +283,14 @@ class Peer:
 
     def update_members(self, their_idea_of_membership):
         self.members = {**self.members, **their_idea_of_membership}
-        # for m in self.members:
-        #     print(m, self.members[m])
 
     @staticmethod
     def start_a_server():
-        print('=== START PEER SERVER ===')
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind(('localhost', 0))  # bind socket to an available port
-        print(f'LISTENING ON {sock.getsockname()}\n')
         sock.listen()
         sock.setblocking(False)  # set non-blocking connections
+        print(f'> PEER SERVER LISTENING ON {sock.getsockname()}\n')
         return sock, sock.getsockname()
 
     @staticmethod
@@ -262,6 +302,12 @@ class Peer:
     def receive(peer, buffer_size=BUF_SZ):
         data = peer.recv(buffer_size)
         return pickle.loads(data)
+
+    @staticmethod
+    def _pick_duration(duration_range):
+        lo, hi = duration_range
+        val = float(random.randint(lo, hi) / 1000.0)
+        return val
 
     @staticmethod
     def pr_now():
@@ -288,6 +334,8 @@ class Peer:
         if self.bully == self.pid:
             return 'self'
         return self.bully
+
+
 
 
 if __name__ == '__main__':
