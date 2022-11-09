14a35ddede9a2ff0729c9a1526d8b6b8efcab871
Duc Vo <dvo4@seattleu.edu>
2022-11-09 15:00:24 -0800

    lab4 - complete chord query with user cli in chord_query.py

--- labs/lab4/chord_query.py
+++ labs/lab4/chord_query.py
@@ -7,6 +7,7 @@ chord_query takes a port number of an existing node and a key
 chord_populate takes a port number of an existing node and the filename 
 of the data file
 """
+import sys
 from enum import Enum
 from hashlib import sha1
 import socket
@@ -14,27 +15,57 @@ from chord_finger import NODES
 import pickle
 
 BUF_SZ = 4096
+ENABLE_CLI = True
+ENABLE_QUERY_INPUT = False
 
 class Method(Enum):
     QUERY = 'QUERY'
+    POPULATE = 'POPULATE'
+    INSERT = 'INSERT'
         
     def is_data_signal(self):
         return self in (Method.POPULATE, Method.INSERT)
    
 class ChordQuery:
-    def __init__(self, port, key):
-        val = self.query(port, self.hash(key))
-        print(val)
-        
     def query(self, port, key, method=Method.QUERY):
         """Query the chord network via an existing node"""
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-            address = ('localhost', port)
+            address = ('localhost', int(port))
             server.connect(address)
-            self.send(server, (method, key, None))
+            self.send(server, (method, self.hash(key), None))
             res = self.receive(server)
+
+            # print('> RESPONSE: {}'.format(res))
+
             server.close()
-            return res
+            return res['message']['value']
+    
+    def run(self):
+        repeat = True
+        while repeat:
+            # user input
+            port = input('> Enter port number of an existing node: ')
+            key = input('> Enter key to query: ')
+            
+            # validation
+            if not port or not key:
+                print('> Error: Invalid input, port, key, must be non-empty')
+                continue
+            try:
+                int(port)
+            except ValueError:
+                print('> Error: Invalid input, port must be an integer')
+                continue
+            
+            # query
+            value = cq.query(port, key)
+            print(f'> KEY: {key}')
+            print(f'> VALUE: {value} \n')
+            repeat = input('Continue? (y/n): ')
+            print('\n')
+            if repeat not in ('y', 'Y', ''):
+                repeat = False
+            
 
     @staticmethod
     def hash(*data: str | int) -> int:
@@ -64,16 +95,19 @@ class ChordQuery:
     
 if __name__ == '__main__':
     port = 43555
-    cq = ChordQuery(port, 'vodangminhduc')
-    repeat = True
-    while repeat:
-        port = input('Enter port number of an existing node: ')
-        key = cq.hash(input('Enter key to query: '))
-        if not port or not key:
-            print('Invalid input, port, key, must be non-empty')
-            continue
-        value = cq.query(int(port), key)
-        print('Value:', value)
-        repeat = input('Continue? (y/n): ')
-        if repeat not in ('y', 'Y', ''):
-            repeat = False
+    key = 'random-key-that-will-be-hashed'
+    
+    if ENABLE_CLI and len(sys.argv) != 3:
+        print('Usage: python chord_query.py <port> <key>')
+        sys.exit(1)
+    elif ENABLE_CLI:
+        port = int(sys.argv[1])
+        key = sys.argv[2]
+    
+    cq = ChordQuery()
+    value = cq.query(port, key)
+    print(f'> KEY: {key}')
+    print(f'> VALUE: {value} \n')
+    
+    # run user interface
+    ENABLE_QUERY_INPUT and cq.run()
