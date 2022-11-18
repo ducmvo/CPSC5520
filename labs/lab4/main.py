c5298714bf4f6b0ec1c8ca89c3ce765b3e2689bf
Duc Vo <dvo4@seattleu.edu>
2022-11-18 15:00:07 -0800

    lab4 - refactor test for chord network

--- labs/lab4/main.py
+++ labs/lab4/main.py
@@ -2,8 +2,9 @@ import time
 import random
 import threading
 from chord_query import ChordQuery as Query
-from chord_node import ChordNode as Node
+from chord_node import ChordNode as Node, ENABLE_JOIN_WITH_DATA
 from chord_populate import ChordPopulate as Populate
+
 
 TEST_BASE = 43544
 FILE_NAME = 'Career_Stats_Passing.csv'
@@ -13,11 +14,13 @@ class ChordTest:
         self.nodes = []
         self.ports = []
         self.threads = []
+        self.ids = []
         for _ in range(num_nodes):
-            node = Node(0)
+            node = self.generate_node()
             self.nodes.append(node)
             self.ports.append(node.address[1])
         
+        # node join random running node
         uports = { self.ports[0] }
         for i, node in enumerate(self.nodes):
             p = self.rand_port(list(uports))
@@ -25,11 +28,22 @@ class ChordTest:
             if i == 0: p = 0
             t = threading.Thread(target=self.serve, args=(node, p))
             self.threads.append(t)
+    
+    def generate_node(self):
+        node = Node(0)
+        # avoid node id collision for different hashed addresses
+        while node.id in self.ids:
+            print("COLLISION", node.id, node.address)
+            node = Node(0)
+        self.ids.append(node.id)
+        return node
             
     def run(self):
         for t in self.threads:
             t.start()
-            time.sleep(0.1)
+            # delay for node to join one at a time
+            # TODO: hanlde concurrent join
+            time.sleep(0.01) 
         
         cq = Query()
         cp = Populate()
@@ -41,9 +55,6 @@ class ChordTest:
         
         # Test parse data from file
         data = cp.parse(FILE_NAME)
-        
-        # Test populate the network via random nodes
-        # cp.populate(self.rand_port(), data)
         
         # Test insert each key-value pair to network
         for key, value in data.items():
@@ -58,6 +69,14 @@ class ChordTest:
         res = cq.query(self.rand_port(), 'Foo')
         test_results += 'TEST 3 PASSED: {}'.format(res.get('value') == 'Bar')
 
+        if ENABLE_JOIN_WITH_DATA:
+            print('{0:=^40}'.format('JOIN AFTER DATA POPULATED'))
+            node = self.generate_node()
+            p = self.rand_port(self.ports)
+            threading.Thread(target=self.serve, args=(node, p)).start()
+            print(f'{"=":=^40}')
+            time.sleep(0.5)
+        
         # Display test results
         print('{0:=^40}'.format('RESULTS'))
         print('NODES', [Node.hash('127.0.0.1', port) for port in self.ports])
@@ -71,10 +90,9 @@ class ChordTest:
     
     @staticmethod    
     def serve(node, port):
-        node.join(port)
-        node.serve()
+        node.run(port)
 
 if __name__ == '__main__':
-    ct = ChordTest(5) # 10 random nodes with random ports
+    ct = ChordTest(5) # 5 random nodes with random ports
     ct.run()
 
